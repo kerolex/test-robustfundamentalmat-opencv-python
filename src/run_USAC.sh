1098279d51e18e3f801a5aeb78b493b5dfee0e20
Alessio <kerolex@users.noreply.github.com>
2022-06-08 18:41:11 +0100

    Update run_USAC.sh

--- src/run_USAC.sh
+++ src/run_USAC.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-C3OD_DIR=$HOME/Desktop/USAC
-DATAPATH=$C3OD_DIR/data/
-RESPATH=$C3OD_DIR/results/
+USAC_DIR=$HOME/Desktop/USAC
+DATAPATH=$USAC_DIR/data/
+RESPATH=$USAC_DIR/results/
 
 DATASET=EuR5
 N_RUNS=100
 
 
 ############################################
-source activate C3OD
+source activate USAC
 
 python test_magsacplusplus.py --respath $RESPATH --datapath $DATAPATH --dataset $DATASET --n_runs $N_RUNS
 
